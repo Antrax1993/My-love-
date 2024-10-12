a8c582ec513585770fbbeded0194c2c6556bd0e5
Antrax1993 <jorgealf226@gmail.com>
2024-10-12 18:34:49 -0400

    Update owner-salir.js

--- plugins/owner-salir.js
+++ plugins/owner-salir.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text, command }) => {
 let id = text ? text : m.chat  
 let pp = 'https://qu.ax/NWVyx.gif'
-await conn.sendMessage(m.chat, { video: { url: pp }, gifPlayback: true, caption: '*Adios a todos, el Bot se despide! 𝑺𝑰𝑺𝑲𝑬𝑫 𝑩𝑶𝑻 - 𝑴𝑫 ⭐', mentions: [m.sender] }, { quoted: m })
+await conn.sendMessage(m.chat, { video: { url: pp }, gifPlayback: true, caption: '*Adios a todos, el Bot se despide! 𝙏𝙃𝙀 𝙅𝙊𝘾𝙆𝙀𝙍 𝘽𝙊𝙏 - 𝑴𝑫 ⭐', mentions: [m.sender] }, { quoted: m })
 await conn.groupLeave(id)}
 handler.help = ['salir']
 handler.tags = ['owner']
