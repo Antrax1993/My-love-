3e59132efbfd77ce813ff282049f91ec8723add3
Antrax1993 <jorgealf226@gmail.com>
2024-10-18 16:45:04 -0400

    Create Jennymors.js

---
+++ Jennymors.js
@@ -0,0 +1,47 @@
+`PLUGUN PLAY YOUTUBE`
+
+import fetch from "node-fetch"
+import yts from 'yt-search'
+
+const handler = async (m, { text, usedPrefix, command, conn }) => {
+    if (!text) {
+      throw m.reply("✧ Ingresa una consulta de *YouTube*");
+    }
+  
+  try {
+    let res = await yts(text);
+    let videoList = res.all;
+    let video = videoList[0];
+
+    let texto = `_*Reproduciendo  ${video.title}...*_`;
+
+    await conn.sendMessage(m?.chat, {react: {text: `🎵`, key: m?.key}});
+
+    await conn.sendMessage(m.chat, { 
+      image: { url: video.thumbnail },  
+      caption: texto 
+    }, { quoted: m });
+
+    let apiUrl = `https://stingy-lari-galaxy-api-309d0e83.koyeb.app/api/youtube?url=${encodeURIComponent(video.url)}`;
+    let result = await (await fetch(apiUrl)).json();
+
+      let audioUrl = result.downloadUrl;
+
+      await conn.sendMessage(m.chat, { 
+        audio: { url: audioUrl }, 
+        mimetype: 'audio/mp4', 
+      }, { quoted: m });
+
+  } catch (error) {
+    console.error(error);
+    m.reply('Error interno, intenta mas tarde.');
+  }
+}
+handler.help = ['pley <consulta>']
+handler.tags = ['downloader']
+handler.command = /^(pley)$/i
+
+handler.premium = false
+handler.register = true
+
+export default handler
